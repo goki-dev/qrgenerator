1abe3db89ac13e1945aa3887a179afe774bd2ee8
unknown <Judoy11!>
2026-04-14 18:35:53 +0800

    eslint ignore dist/ and node_modules. this is for github actions

--- eslint.config.js
+++ eslint.config.js
@@ -4,19 +4,28 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
+
+  // ⭐ CUSTOM FIX (ADD THIS BLOCK)
+  {
+    ignores: ["dist/**", "node_modules/**"]
+  },
+
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.browser }
   },
+
   pluginReact.configs.flat.recommended,
+
   {
     rules: {
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off'
     }
   },
+
   {
     files: ["**/*.test.{js,jsx}"],
     languageOptions: {
